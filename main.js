11196e90080b957fd1c9e794525afff26066ecd9
PaiGit759 <107637697+PaiGit759@users.noreply.github.com>
2022-09-06 11:36:19 +0200

    20220906 11:36

--- main.js
+++ main.js
@@ -2,22 +2,22 @@ let stringBase64;
 let stringStorage;
 var file;
 var prstr;
-var listCats = 'Barcelona~1|Everest~2|Tom~3|Rafail~4|Maya~5';
+var listCats;//= 'Barcelona~1|Everest~2|Tom~3|Rafail~4|Maya~5';
+var storedFotoValue;
+var storedDateValue;
 
 function encodeImageFileAsURL(element) {
-   file = element.files[0];
+  file = element.files[0];
   var reader = new FileReader();
-  reader.onloadend = function() {
+  reader.onloadend = function () {
     console.log(file.name, reader.result)
     stringBase64 = reader.result
     document.getElementById("foto").src = stringBase64;
-
   }
   reader.readAsDataURL(file);
 }
 
 function UploadFile(index = 0) {
-  //  alert(file.size)
   index = document.getElementById("indexOnloadFile").value;
 
   stringStorage = localStorage.getItem('storedFotoValue');
@@ -26,130 +26,156 @@ function UploadFile(index = 0) {
     alert("Index is outside the array")
     return
   }
-       document.getElementById("foto").src = arr[index];//stringBase64;
-   }
+  document.getElementById("foto").src = arr[index];//stringBase64;
+}
 
 function buttonOnclickUpload(index = 2) {
-stringStorage = localStorage.getItem('storedFotoValue');
-arr = stringStorage.split(' ');
-     document.getElementById("foto").src = arr[index];//stringBase64;
- }
- 
- function buttonOnclickSave() {
-  if (file.size>=100000) {
+  stringStorage = localStorage.getItem('storedFotoValue');
+  arr = stringStorage.split(' ');
+  document.getElementById("foto").src = arr[index];//stringBase64;
+}
+
+function buttonOnclickSave() {
+  if (file.size >= 100000) {
     alert("File size must not exceed 100 KB");
     return;
   }
-  //localStorage.setItem('storedFotoValue',"" );
-
   stringStorage = localStorage.getItem('storedFotoValue') + " " + stringBase64;
   arr = stringStorage.split(' ');
 
-  localStorage.setItem('storedFotoValue',stringStorage);
-  aaa = 9;
-//       document.getElementById("foto").src = stringBase64;
-   }
-
+  localStorage.setItem('storedFotoValue', stringStorage);
+}
 
 // menu
-
 
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-window.onclick = function(event) {
+window.onclick = function (event) {
 
   catId = event.target.id;
 
-
-
   if (catId.includes('elCat')) {
-    strIndex = catId.replace('elCat','');
-    alert(catId + '=' + strIndex)
+    strIndex = catId.replace('elCat', '');
+    //    alert(catId + '=' + strIndex)
+    //выведем данные кошек
+
+    listCats = localStorage.getItem('listCats');
+    arr = listCats.split('|');
+    arr1 = arr[strIndex].split('~');
+
+    catName = arr1[0];
+    aaa = 9;
+
+    document.getElementById("catName").value = catName;
+
+    storedDateValue = localStorage.getItem('storedDateValue');
+    arr = storedDateValue.split('|');
+    arr1 = arr[strIndex].split('~');
+
+    document.getElementById("catDateOfBirth").value = arr1[0];
+    document.getElementById("catWhere").value = arr1[1];
+    document.getElementById("catBreed").value = arr1[2];
+    document.getElementById("catColor").value = arr1[3];
+    document.getElementById("catWeight").value = arr1[4];
+
+    storedFotoValue = localStorage.getItem('storedFotoValue');
+    arr = storedFotoValue.split(' ');
+    document.getElementById("foto").src = arr[1];
+
+
+    aaa = 9;
   };
-  
-      if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-          var openDropdown = dropdowns[i];
-          if (openDropdown.classList.contains('show')) {
-            openDropdown.classList.remove('show');
-          }
-        }
+
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
-
-    
-function changeListMenuCat()
-{ 
-listMenuCat = document.getElementById("myDropdown");
-listCats = localStorage.getItem('listCats');
-arr = listCats.split('|');
-arr.sort();
-aaa = 9;
-prstr = "";
-
-/* for (arrstr of arr) {
-  arr1 = arrstr.split('~');
-  prstr = prstr + '<a href="#" id="elCat' + arr1[1] + '" > ' + arr1[0] + '</a>';
-  aaa = 9;
-
-} */
-
-for (i=1; i <arr.length; i++) {
-  arr1 = arr[i].split('~');
-  prstr = prstr + '<a href="#" id="elCat' + arr1[1] + '" > ' + arr1[0] + '</a>';
-  aaa = 9;
-
+  }
 }
 
-listMenuCat.innerHTML =  prstr;
-aaa = 9;
+
+function changeListMenuCat() {
+  listMenuCat = document.getElementById("myDropdown");
+  listCats = localStorage.getItem('listCats');
+  arr = listCats.split('|');
+  arr.sort();
+  prstr = "";
+
+  for (i = 1; i < arr.length; i++) {
+    arr1 = arr[i].split('~');
+    prstr = prstr + '<a href="#" id="elCat' + arr1[1] + '" > ' + arr1[0] + '</a>';
+
+  }
+  listMenuCat.innerHTML = prstr;
 }
 
-function onclickVsubmit()
-{
-  
-//  alert('submit!') reset() formForCats
+function onclickVsubmit() {
 
-listCats = localStorage.getItem('listCats');
-catName = document.getElementById("catName").value ;
-arr = listCats.split('|');
-listCats = localStorage.getItem('listCats');
-listCats = listCats +  '|' + catName + '~' + (arr.length);// 'Barcelona~1|Everest~2|Tom~3|Rafail~4|Maya~5';
- localStorage.setItem('listCats', listCats);
-changeListMenuCat();
+  //   список кошек
+  listCats = localStorage.getItem('listCats');
+  catName = document.getElementById("catName").value;
+  arr = listCats.split('|');
+  listCats = localStorage.getItem('listCats');
+  listCats = listCats + '|' + catName + '~' + (arr.length);
+  localStorage.setItem('listCats', listCats);
+  changeListMenuCat();
 
-formForCats = document.getElementById("formForCats").reset();
-document.getElementById("foto").src = "";
+  index = arr.length + 1;
 
-document.getElementById("submitButton").style = 'display: none;';
-document.getElementById("onloadfile").style = 'display: none;';
+  //   список данных кошек 
+  storedDateValue = localStorage.getItem('storedDateValue');
+
+  catDateOfBirth = document.getElementById("catDateOfBirth").value;
+  catWhere = document.getElementById("catWhere").value;
+  catBreed = document.getElementById("catBreed").value;
+  catColor = document.getElementById("catColor").value;
+  catWeight = document.getElementById("catWeight").value;
+
+  storedDateValue = storedDateValue + '|' + catDateOfBirth + '~' + catWhere + '~' + catBreed + '~' + catColor + '~' + catWeight;
+  localStorage.setItem('storedDateValue', storedDateValue);
+
+  //   список фото кошек 
+  if (file == undefined) {
+    storedFotoValue = localStorage.getItem('storedFotoValue') + "no_foto";
+    localStorage.setItem('storedFotoValue', storedFotoValue);
+    formForCats = document.getElementById("formForCats").reset();
+    document.getElementById("foto").src = "";
+
+  }
+
+  if (file.size >= 100000) {
+    alert("File size must not exceed 100 KB");
+  }
+  else {
+    storedFotoValue = localStorage.getItem('storedFotoValue') + " " + stringBase64;
+    localStorage.setItem('storedFotoValue', storedFotoValue);
+  }
+
+  formForCats = document.getElementById("formForCats").reset();
+  document.getElementById("foto").src = "";
+
+  document.getElementById("submitButton").style = 'display: none;';
+  document.getElementById("onloadfile").style = 'display: none;';
 
 }
 
 function buttonOnclickUpAddCat() {
+
+  formForCats = document.getElementById("formForCats").reset();
   document.getElementById("submitButton").style = 'display: inline-block;';
   document.getElementById("onloadfile").style = 'display: inline-block;';
 
 }
-
-// localStorage.setItem('listCats', listCats);
-// localStorage.setItem('listCats', "");
-
-
+/* 
+ localStorage.setItem('listCats', "");
+localStorage.setItem('storedFotoValue',"");
+localStorage.setItem('storedDateValue',""); 
+ */
 changeListMenuCat();
-
-// localStorage.setItem('storedValue', localStorage.getItem('storedValue') + "|" + storedValue);
-  
-// document.getElementById("exsamp").innerHTML = "<br><br>" + localStorage.getItem('storedValue');
-
-
-
-//document.getElementById("fildForInput").value = localStorage.getItem('inputValueH');
-//document.getElementById("fildForInput1").value = localStorage.getItem('inputValueR');
-
-//localStorage.setItem('storedValue', "");
-//aaa = document.getElementById("storedValue").innerHTML = "**********";
